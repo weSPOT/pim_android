8b84dc7b7142a7a12a81e65701d21e8b44e5da08
titogelo <titogelo@gmail.com>
2014-10-16 14:25:29 +0200

    logout offline

--- src/net/wespot/pim/utils/generic/NetworkHandle.java
+++ src/net/wespot/pim/utils/generic/NetworkHandle.java
@@ -1,5 +1,11 @@
 package net.wespot.pim.utils.generic;
 
+import android.os.Handler;
+import android.os.Message;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.celstec.arlearn2.android.delegators.ARL;
+
 /**
  * ****************************************************************************
  * Copyright (C) 2014 Open Universiteit Nederland
@@ -23,4 +29,78 @@ package net.wespot.pim.utils.generic;
  */
 
 public class NetworkHandle {
+
+    public void executeTest() {
+        isNetworkAvailable(h,2000);
+    }
+
+    public class NetworkResult{
+        private boolean result;
+
+        public NetworkResult(boolean result) {
+            this.result = result;
+        }
+
+        public boolean isResult() {
+            return result;
+        }
+
+        public void setResult(boolean result) {
+            this.result = result;
+        }
+    }
+
+    public static void isNetworkAvailable(final Handler handler, final int timeout) {
+        // ask fo message '0' (not connected) or '1' (connected) on 'handler'
+        // the answer must be send before before within the 'timeout' (in milliseconds)
+
+        new Thread() {
+            private boolean responded = false;
+            @Override
+            public void run() {
+                // set 'responded' to TRUE if is able to connect with google mobile (responds fast)
+                new Thread() {
+                    @Override
+                    public void run() {
+                        HttpGet requestForTest = new HttpGet("http://m.google.com");
+                        try {
+                            new DefaultHttpClient().execute(requestForTest); // can last...
+                            responded = true;
+                        }
+                        catch (Exception e) {
+                        }
+                    }
+                }.start();
+
+                try {
+                    int waited = 0;
+                    while(!responded && (waited < timeout)) {
+                        sleep(100);
+                        if(!responded ) {
+                            waited += 100;
+                        }
+                    }
+                }
+                catch(InterruptedException e) {} // do nothing
+                finally {
+                    if (!responded) { handler.sendEmptyMessage(0); }
+                    else { handler.sendEmptyMessage(1); }
+                }
+            }
+        }.start();
+    }
+
+    Handler h = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            if (msg.what != 1) { // code if not connected
+                ARL.eventBus.post(new NetworkResult(false));
+
+            } else { // code if connected
+                ARL.eventBus.post(new NetworkResult(true));
+
+            }
+        }
+    };
 }
