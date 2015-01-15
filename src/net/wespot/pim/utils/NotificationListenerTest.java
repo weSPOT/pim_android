03d309be12e96f6ef8d2bc5e801ea673c6599da2
titogelo <titogelo@gmail.com>
2015-01-15 19:37:22 +0100

    first approach for push notifications

--- src/net/wespot/pim/utils/NotificationListenerTest.java
+++ src/net/wespot/pim/utils/NotificationListenerTest.java
@@ -1,6 +1,11 @@
 package net.wespot.pim.utils;
 
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import net.wespot.pim.R;
+import org.celstec.arlearn2.android.delegators.ARL;
 import org.celstec.arlearn2.android.gcm.NotificationListenerInterface;
 
 import java.util.HashMap;
@@ -26,13 +31,50 @@ import java.util.HashMap;
  * ****************************************************************************
  */
 public class NotificationListenerTest implements NotificationListenerInterface {
+    private static final java.lang.String TAG = "12312";
+
+//    @Override
+//    public boolean acceptNotificationType(String notificationType) {
+//        return true;
+//    }
+//
+//    @Override
+//    public void handleNotification(HashMap<String, String> map) {
+//        Log.e(TAG, "testtttt");
+//        ARL.getContext();
+//    }
+
+//    /**
+//     * Creates an IntentService.  Invoked by your subclass's constructor.
+//     *
+//     * @param name Used to name the worker thread, important only for debugging.
+//     */
+//    public NotificationListenerTest(String name) {
+//        super(name);
+//    }
+//
     @Override
     public boolean acceptNotificationType(String notificationType) {
-         return true;
+         return false;
     }
 
     @Override
     public void handleNotification(HashMap map) {
+        System.out.println("map = " + map);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(ARL.getContext())
+                        .setSmallIcon(R.drawable.common_signin_btn_icon_dark)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
 
+        NotificationManager mNotificationManager =
+                (NotificationManager) ARL.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(Integer.parseInt(TAG), mBuilder.build());        mNotificationManager.notify(Integer.parseInt(TAG), mBuilder.build());
     }
+
+//    @Override
+//    protected void onHandleIntent(Intent intent) {
+//        Log.e(TAG, "test")
+//    }
 }
--- src/net/wespot/pim/view/InqCommunicateFragment.java
+++ src/net/wespot/pim/view/InqCommunicateFragment.java
@@ -21,11 +21,15 @@ package net.wespot.pim.view;
  * ****************************************************************************
  */
 
+import android.app.ActivityManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.InputType;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -36,40 +40,50 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import daoBase.DaoConfiguration;
+import net.wespot.pim.MainActivity;
 import net.wespot.pim.R;
 import net.wespot.pim.controller.Adapters.ChatAdapter;
-import net.wespot.pim.utils.RetrieveMessageTask;
-import net.wespot.pim.utils.TimeMessageEvent;
+import net.wespot.pim.controller.InquiryPhasesActivity;
 import org.celstec.arlearn.delegators.INQ;
 import org.celstec.arlearn2.android.delegators.ARL;
 import org.celstec.arlearn2.android.events.MessageEvent;
-import org.celstec.dao.gen.MessageLocalObject;
-import org.celstec.dao.gen.MessageLocalObjectDao;
+import org.celstec.arlearn2.android.gcm.NotificationListenerInterface;
+import org.celstec.dao.gen.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class InqCommunicateFragment extends Fragment {
+public class InqCommunicateFragment extends Fragment implements NotificationListenerInterface {
 
     private static final String TAG = "InqCommunicateFragment";
+    private static final String NUMBER = "0123";
     private EditText message;
     private ImageButton send;
     private View rootView;
 
     private ListView listViewMessages;
 
+    private int numMessages = 0;
+    private NotificationCompat.Builder mBuilder;
+    private NotificationManager mNotificationManager;
+    private NotificationCompat.InboxStyle mNotificationStyle;
+
+
     ChatAdapter chatAdapter;
     private HashMap<MessageLocalObject, View> messages_views = new HashMap<MessageLocalObject, View>();
     ArrayList<MessageLocalObject> messages;
 
-    public static Timer timer;
+//    public static Timer timer;
 
-    List<MessageLocalObject> messageLocalObjectList;
-    List<MessageLocalObject> messageLocalObjectList_newMessages;
+//    private ViewGroup mContainerView;
+//    private static final long INTERVAL = 5; /* seconds */
+
+    private List<MessageLocalObject> messageLocalObjectList;
+    private List<MessageLocalObject> messageLocalObjectList_newMessages;
 
     Map<String, String> accountNamesID = new HashMap<String, String>();
-
-    private ViewGroup mContainerView;
-    private static final long INTERVAL = 5; /* seconds */
 
 
     public InqCommunicateFragment() {
@@ -162,17 +176,6 @@ public class InqCommunicateFragment extends Fragment {
         return rootView;
     }
 
-    public static void disableSoftKeyboard(final EditText v) {
-        v.setInputType(InputType.TYPE_NULL);
-        if (Build.VERSION.SDK_INT >= 11) {
-            v.setRawInputType(InputType.TYPE_CLASS_TEXT);
-            v.setTextIsSelectable(true);
-        } else {
-            v.setRawInputType(InputType.TYPE_NULL);
-            v.setFocusable(true);
-        }
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -193,10 +196,6 @@ public class InqCommunicateFragment extends Fragment {
                 .orderAsc(MessageLocalObjectDao.Properties.Time)
                 .list();
 
-        timer = new Timer();
-        timer.schedule(new RetrieveMessageTask(), INTERVAL * 1000, INTERVAL * 1000);
-        Log.e(TAG, "timer");
-
         for (MessageLocalObject messageLocalObject : messageLocalObjectList) {
             messages.add(messageLocalObject);
         }
@@ -204,48 +203,284 @@ public class InqCommunicateFragment extends Fragment {
         chatAdapter = new ChatAdapter(getActivity(), messages, messages_views);
     }
 
-    public synchronized void onEventMainThread(TimeMessageEvent timeMessageEvent) {
-        Log.i(TAG, "retrieve messages");
+    public void onEventMainThread(MessageEvent messageEvent) {
+        Log.e(TAG, "message synced: " + messageEvent.getRunId());
 
-        INQ.messages.syncMessagesForDefaultThread(INQ.inquiry.getCurrentInquiry().getRunId());
+        receiveMessage(messageEvent.getRunId());
 
-        messageLocalObjectList_newMessages = DaoConfiguration.getInstance().getMessageLocalObject().queryBuilder()
+
+
+//                            ActivityManager activityManager = (ActivityManager) ARL.getContext().getSystemService(Context.ACTIVITY_SERVICE);
+//                            List<ActivityManager.RunningTaskInfo> services = activityManager
+//                                    .getRunningTasks(Integer.MAX_VALUE);
+//                            boolean isActivityFound = false;
+
+//                            if (services.get(0).topActivity.getPackageName().toString()
+//                                    .equalsIgnoreCase(ARL.getContext().getPackageName().toString())) {
+//                                isActivityFound = true;
+//                            }
+//
+//                            if (isActivityFound) {
+//                                Log.e(TAG, "pim opened");
+////                    if (!messages_views.containsKey(messageLocalObject)){
+////                        messages.add(messageLocalObject);
+////                    }else{
+////                        ViewGroup viewUpdate = (ViewGroup) chatAdapter.getViewFromMessage(messageLocalObject);
+////                        if (viewUpdate != null){
+////                            viewUpdate.inflate(getActivity(),R.layout.entry_messages, null);
+////                            viewUpdate.invalidate();
+////                        }
+////                    }
+//                            } else {
+//                                Log.e(TAG, "pim closed");
+//                                // int number_messages_before = messageLocalObjectList.size();
+////                                createNotification(messageLocalObject.getBody());
+//                            }
+//            }
+//        }
+
+//                    chatAdapter.notifyDataSetChanged();
+
+
+//
+//
+//
+//                    messageLocalObjectList_newMessages = DaoConfiguration.getInstance().getMessageLocalObject().queryBuilder()
+//                            .where(MessageLocalObjectDao.Properties.RunId.eq(INQ.inquiry.getCurrentInquiry().getRunId()))
+//                            .orderAsc(MessageLocalObjectDao.Properties.Time)
+//                            .list();
+//
+//                    if (messageLocalObjectList_newMessages.size() != num){
+//
+//
+//
+//                        // To know if the PIM is running or not
+//                        ActivityManager activityManager = (ActivityManager) ARL.getContext().getSystemService(Context.ACTIVITY_SERVICE);
+//                        List<ActivityManager.RunningTaskInfo> services = activityManager
+//                                .getRunningTasks(Integer.MAX_VALUE);
+//                        boolean isActivityFound = false;
+//
+//                        if (services.get(0).topActivity.getPackageName().toString()
+//                                .equalsIgnoreCase(ARL.getContext().getPackageName().toString())) {
+//                            isActivityFound = true;
+//                        }
+////
+//                        messageLocalObjectList_newMessages.removeAll(messageLocalObjectList);
+//                        for (MessageLocalObject messageLocalObject : messageLocalObjectList_newMessages) {
+//                            // If it is running we don't show the notification
+//                            if (isActivityFound) {
+//                                Log.e(TAG, "si");
+//                        //                    if (!messages_views.containsKey(messageLocalObject)){
+//                        //                        messages.add(messageLocalObject);
+//                        //                    }else{
+//                        //                        ViewGroup viewUpdate = (ViewGroup) chatAdapter.getViewFromMessage(messageLocalObject);
+//                        //                        if (viewUpdate != null){
+//                        //                            viewUpdate.inflate(getActivity(),R.layout.entry_messages, null);
+//                        //                            viewUpdate.invalidate();
+//                        //                        }
+//                        //                    }
+//                            } else {
+//                                // int number_messages_before = messageLocalObjectList.size();
+//                                createNotification(messageLocalObject.getBody());
+//                            }
+//
+//                            messageLocalObjectList.add(messageLocalObject);
+//                            messageLocalObjectList_newMessages.remove(messageLocalObject);
+//                        }
+//                        chatAdapter.notifyDataSetChanged();
+//                    }
+
+    }
+
+    private void receiveMessage(Long runId) {
+        InquiryLocalObject inquiryLocalObject = DaoConfiguration.getInstance().getInquiryLocalObjectDao().queryBuilder().where(
+                InquiryLocalObjectDao.Properties.RunId.eq(runId)
+        ).list().get(0);
+
+        if (!inquiryLocalObject.equals(null)){
+            INQ.inquiry.setCurrentInquiry(inquiryLocalObject);
+        }
+
+        messageLocalObjectList = DaoConfiguration.getInstance().getMessageLocalObject().queryBuilder()
                 .where(MessageLocalObjectDao.Properties.RunId.eq(INQ.inquiry.getCurrentInquiry().getRunId()))
                 .orderAsc(MessageLocalObjectDao.Properties.Time)
                 .list();
 
-        if (messageLocalObjectList_newMessages.size() != messageLocalObjectList.size()){
+        if (isPimRunning()){
+            for (MessageLocalObject messageLocalObject : messageLocalObjectList) {
+                if (messageLocalObject.getRead() == null) {
 
-            messageLocalObjectList_newMessages.removeAll(messageLocalObjectList);
-            for (MessageLocalObject messageLocalObject : messageLocalObjectList_newMessages) {
-
-                if (!messages_views.containsKey(messageLocalObject)){
-                    messages.add(messageLocalObject);
-                }else{
-                    ViewGroup viewUpdate = (ViewGroup) chatAdapter.getViewFromMessage(messageLocalObject);
-                    if (viewUpdate != null){
-                        viewUpdate.inflate(getActivity(),R.layout.entry_messages, null);
-                        viewUpdate.invalidate();
+                    // No notification but we need to update the view
+                    if (!messages_views.containsKey(messageLocalObject)){
+                        messages.add(messageLocalObject);
+                    }else{
+                        ViewGroup viewUpdate = (ViewGroup) chatAdapter.getViewFromMessage(messageLocalObject);
+                        if (viewUpdate != null){
+                            viewUpdate.inflate(getActivity(),R.layout.entry_messages, null);
+                            viewUpdate.invalidate();
+                        }
                     }
+
+                    chatAdapter.notifyDataSetChanged();
+
+                    messageLocalObject.setRead(true);
+                    DaoConfiguration.getInstance().getMessageLocalObject().insertOrReplace(messageLocalObject);
                 }
-                messageLocalObjectList.add(messageLocalObject);
-                messageLocalObjectList_newMessages.remove(messageLocalObject);
             }
+        }else{
+            for (MessageLocalObject messageLocalObject : messageLocalObjectList) {
+                if (messageLocalObject.getRead() == null) {
 
-            chatAdapter.notifyDataSetChanged();
+                    createNotification(messageLocalObject);
+
+                    messageLocalObject.setRead(true);
+
+                    DaoConfiguration.getInstance().getMessageLocalObject().insertOrReplace(messageLocalObject);
+                }
+            }
         }
-    }
-
-    public void onEventBackgroundThread(MessageEvent messageEvent) {
-        Log.e(TAG, "message synced: " + messageEvent.getRunId());
     }
 
     @Override
     public void onDestroy() {
         ARL.eventBus.unregister(this);
-        timer.cancel();
         super.onDestroy();
     }
+
+    public boolean isPimRunning(){
+        ActivityManager activityManager = (ActivityManager) ARL.getContext().getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> services = activityManager
+                .getRunningTasks(Integer.MAX_VALUE);
+        boolean isActivityFound = false;
+
+        if (services.get(0).topActivity.getPackageName().toString()
+                .equalsIgnoreCase(ARL.getContext().getPackageName().toString())) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    @Override
+    public boolean acceptNotificationType(String notificationType) {
+        return true;
+    }
+
+    @Override
+    public void handleNotification(HashMap<String, String> map) {
+        if (map.containsKey("type")) {
+            if (map.containsValue("org.celstec.arlearn2.beans.notification.MessageNotification")) {
+                Log.i(TAG, "retrieve messages");
+                if (!ARL.eventBus.isRegistered(this)){
+                    ARL.eventBus.register(this);
+                }
+
+                INQ.runs.syncRun(Long.parseLong(map.get("runId")));
+                RunLocalObject runLocalObject = null;
+                do {
+                    runLocalObject  = DaoConfiguration.getInstance().getRunLocalObjectDao().load( Long.parseLong(map.get("runId")) );
+                }while(runLocalObject == null);
+
+                INQ.messages.syncMessagesForDefaultThread(runLocalObject.getId());
+            }
+        }
+    }
+
+    private void createNotification(MessageLocalObject messageLocalObject) {
+
+        if (mBuilder == null){
+
+            if (mNotificationStyle == null){
+                mNotificationStyle = new NotificationCompat.InboxStyle();
+            }
+            mBuilder = new NotificationCompat.Builder(ARL.getContext())
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    .setContentTitle("My notification")
+                    .setAutoCancel(true)
+                    .setSortKey("0")
+                    .setStyle(mNotificationStyle
+                                    .addLine(messageLocalObject.getAuthor()+": "+messageLocalObject.getBody())
+                                    .setSummaryText(++numMessages + " new messages")
+                    )
+            ;
+            // Creates an explicit intent for an Activity in your app
+            Intent resultIntent = new Intent(ARL.getContext(), InquiryPhasesActivity.class);
+
+
+            // The stack builder object will contain an artificial back stack for the
+            // started Activity.
+            // This ensures that navigating backward from the Activity leads out of
+            // your application to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(ARL.getContext());
+            // Adds the back stack for the Intent (but not the Intent itself)
+            stackBuilder.addParentStack(MainActivity.class);
+            // Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+        }else{
+            mNotificationStyle
+                    .addLine(messageLocalObject.getAuthor() + ": " + messageLocalObject.getBody())
+                    .setSummaryText(++numMessages + " new messages");
+        }
+
+        if (mNotificationManager == null){
+            mNotificationManager = (NotificationManager) ARL.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(Integer.parseInt(NUMBER), mBuilder.build());
+    }
+
+
+//    public synchronized void onEventMainThread(TimeMessageEvent timeMessageEvent) {
+////        Log.i(TAG, "retrieve messages");
+////
+////        INQ.messages.syncMessagesForDefaultThread(INQ.inquiry.getCurrentInquiry().getRunId());
+////
+////        messageLocalObjectList_newMessages = DaoConfiguration.getInstance().getMessageLocalObject().queryBuilder()
+////                .where(MessageLocalObjectDao.Properties.RunId.eq(INQ.inquiry.getCurrentInquiry().getRunId()))
+////                .orderAsc(MessageLocalObjectDao.Properties.Time)
+////                .list();
+////
+////        if (messageLocalObjectList_newMessages.size() != messageLocalObjectList.size()){
+////
+////            messageLocalObjectList_newMessages.removeAll(messageLocalObjectList);
+////            for (MessageLocalObject messageLocalObject : messageLocalObjectList_newMessages) {
+////
+////                if (!messages_views.containsKey(messageLocalObject)){
+////                    messages.add(messageLocalObject);
+////                }else{
+////                    ViewGroup viewUpdate = (ViewGroup) chatAdapter.getViewFromMessage(messageLocalObject);
+////                    if (viewUpdate != null){
+////                        viewUpdate.inflate(getActivity(),R.layout.entry_messages, null);
+////                        viewUpdate.invalidate();
+////                    }
+////                }
+////                messageLocalObjectList.add(messageLocalObject);
+////                messageLocalObjectList_newMessages.remove(messageLocalObject);
+////            }
+////
+////            chatAdapter.notifyDataSetChanged();
+////        }
+//    }
+
+//    public static void disableSoftKeyboard(final EditText v) {
+//        v.setInputType(InputType.TYPE_NULL);
+//        if (Build.VERSION.SDK_INT >= 11) {
+//            v.setRawInputType(InputType.TYPE_CLASS_TEXT);
+//            v.setTextIsSelectable(true);
+//        } else {
+//            v.setRawInputType(InputType.TYPE_NULL);
+//            v.setFocusable(true);
+//        }
+//    }
 
 //    private class SendMessage extends AsyncTask<Void, String, String>
 //    {
@@ -293,5 +528,9 @@ public class InqCommunicateFragment extends Fragment {
 //            }
 //        }
 //    }
+
+
 }
+
+
 
